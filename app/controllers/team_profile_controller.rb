d3c6cdfa8b2d5063fa2c5250b93f2ac64199e816
mahtai <matthewahtai@gmail.com>
2015-12-02 14:20:21 -0500

    Update team_profile_controller.rb

--- app/controllers/team_profile_controller.rb
+++ app/controllers/team_profile_controller.rb
@@ -1,1 +1,24 @@
+class Team_ProfileController < ApplicationController
+    
+    def new
+        @team_profile = Team_Profile.new
+    end
+    
+    def create
+        @team_profile = Team_Profile.new(profile_params)
+    
+        if @team_profile.save
+            redirect_to @team_profile
+        else 
+            render 'new'
+        end
+    end    
+    
+    def show
+        @team_profile = Team_Profile.find(params[:id])
+    end
 
+private
+    def team_profile_params
+        params.require(:team_profile).permit(:name)
+    end
