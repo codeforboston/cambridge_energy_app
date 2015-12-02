2d9d8d9d9df322f7304854f59fcda8966ac7c0b9
mahtai <matthewahtai@gmail.com>
2015-12-02 14:10:56 -0500

    Update and rename building_profile_controller.rb to user_building_profile_controller.rb

---
+++ app/controllers/user_building_profile_controller.rb
@@ -0,0 +1,24 @@
+class User_Building_ProfileController < ApplicationController
+    
+    def new
+        @user_building_profile = User_Building_Profile.new
+    end
+    
+    def create
+        @user_building_profile = User_Building_Profile.new(profile_params)
+    
+        if @user_building_profile.save
+            redirect_to @user_building_profile
+        else 
+            render 'new'
+        end
+    end    
+    
+    def show
+        @user_building_profile = User_Building_Profile.find(params[:id])
+    end
+
+private
+    def user_profile_params
+        params.require(:user_building_profile).permit(STUFF)
+    end
