84a34cbe35c755d53191c0a360ef7edcd8a2569f
mahtai <matthewahtai@gmail.com>
2015-12-02 14:37:15 -0500

    Update unit_profile.rb

--- app/models/unit_profile.rb
+++ app/models/unit_profile.rb
@@ -1,6 +1,6 @@
 class Unit_Profile < ActiveRecord::Base
   validates :building_id
   validates :unit_number
-  validates :sqfootage, Numericality: true, greater_than: 0
+  validates :sqfootage, numericality: true, greater_than: 0
   validates :appliances
 end
