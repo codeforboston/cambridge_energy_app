c1ff215e1fbea88373854cf6f8303aa8e4cb58d5
mahtai <matthewahtai@gmail.com>
2015-12-02 15:00:25 -0500

    Update user_profile.rb

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,10 +1,13 @@
 class User_Profile < ActiveRecord::Base
-    validates :first_name
-    validates :last_name
+    before_save { self.email = email.downcase }
+    validates :first_name, length: { maximum: 50 }
+    validates :last_name, length: { maximum: 50 }
     validates :street_address, presence: true
     validates :unit_id
     validates :phone
-    validates :email, presence: true
-                                uniqueness: {true, message: "Email address already listed."}
+    validates :email, presence: true, length: { maximum: 255 }
+                      format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+                      uniqueness: { case_sensitive: false }
     validates :team_id
+    validates :password, presence: true
 end
