5470e886ad6612a01b7a942dd020cd936df8f2fc
mahtai <matthewahtai@gmail.com>
2015-12-02 14:29:33 -0500

    Update user_profile.rb

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,12 +1,10 @@
 class User_Profile < ActiveRecord::Base
-    validates :first_name, presence: true %first name of user
-    validates :last_name, presence: true %last name of user
-    validates :streetAddress, presence: true,
-                                format: { with: REGEXP , message: "Not valid street address." },
-                                uniqueness: {true, message: "Street address already listed."} %street address of user
-    validates :aptNumber
-    validates :emailAddress, presence: true,
-                                format: { with: REGEXP, message: "Not valid email address." },
-                                uniqueness: {true, message: "Email address already listed."} %email address of user
-    validates :team_id %id of team or buddy
+    validates :firstname
+    validates :lastname
+    validates :streetAddress, presence: true
+    validates :unit_id
+    validates :phone
+    validates :email, presence: true
+                                uniqueness: {true, message: "Email address already listed."}
+    validates :team_id
 end
