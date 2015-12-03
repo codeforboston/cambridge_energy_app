1fd13ee3cce1ce9801b9d7ee20645b147868f78a
mahtai <matthewahtai@gmail.com>
2015-12-03 13:20:26 -0500

    Update user_building_profile_controller.rb

--- app/controllers/user_building_profile_controller.rb
+++ app/controllers/user_building_profile_controller.rb
@@ -1,24 +1,24 @@
-class User_Building_ProfileController < ApplicationController
+class UserBuildingController < ApplicationController
     
     def new
-        @user_building_profile = User_Building_Profile.new
+        @user_building = UserBuilding.new
     end
     
     def create
-        @user_building_profile = User_Building_Profile.new(user_building_profile_params)
+        @user_building = UserBuilding.new(user_building_params)
     
-        if @user_building_profile.save
-            redirect_to @user_building_profile
+        if @user_building.save
+            redirect_to @user_building
         else 
             render 'new'
         end
     end    
     
     def show
-        @user_building_profile = User_Building_Profile.find(params[:id])
+        @user_building = UserBuilding.find(params[:id])
     end
 
 private
-    def user_building_profile_params
-        params.require(:user_building_profile).permit(:address, :lat, :lon)
+    def user_building_params
+        params.require(:user_building).permit(:address, :lat, :lon)
     end
