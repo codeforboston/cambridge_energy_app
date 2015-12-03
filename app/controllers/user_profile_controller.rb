17f60c237a670de7313dfca94766b48086054e77
mahtai <matthewahtai@gmail.com>
2015-12-03 13:21:07 -0500

    Update user_profile_controller.rb

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -1,24 +1,24 @@
-class User_ProfileController < ApplicationController
+class UserController < ApplicationController
     
     def new
-        @user_profile = User_Profile.new
+        @user = User.new
     end
     
     def create
-        @user_profile = User_Profile.new(user_profile_params)
+        @user = User.new(user_params)
     
-        if @user_profile.save
-            redirect_to @user_profile
+        if @user.save
+            redirect_to @user
         else 
             render 'new'
         end
     end    
     
     def show
-        @user_profile = User_Profile.find(params[:id])
+        @user = User.find(params[:id])
     end
 
 private
-    def user_profile_params
-        params.require(:user_profile).permit(:first_name, :last_name, :street_address, :unit_id, :phone, :email, :team_id, :password)
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :street_address, :unit_id, :phone, :email, :team_id, :password)
     end
