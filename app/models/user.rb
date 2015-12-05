75b2541686c680cb7e3e55120cc73601acb468b1
mahtai <matthewahtai@gmail.com>
2015-12-05 18:50:22 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
     before_save { self.email = email.downcase }
+    belongs_to :unit
+    belongs_to :team
     validates :first_name, length: { maximum: 50 }
     validates :last_name, length: { maximum: 50 }
     validates :street_address, presence: true
-    validates :unit_id
     validates :phone
     validates :email, presence: true, length: { maximum: 255 }
                       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
                       uniqueness: { case_sensitive: false }
-    validates :team_id
     validates :password, presence: true
 end
