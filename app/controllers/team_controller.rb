531af1b5c7d0d0ff87432c4359466a6e5ddb6602
mahtai <matthewahtai@gmail.com>
2015-12-05 22:04:02 -0500

    Update team_controller.rb

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,9 +1,21 @@
 class TeamController < ApplicationController
     
+    def index
+        @team = Team.all
+    end
+
+    def show
+        @team = Team.find(params[:id])
+    end
+
     def new
         @team = Team.new
     end
-    
+
+    def edit
+        @team = Team.find(params[:id])
+    end
+
     def create
         @team = Team.new(team_params)
     
@@ -12,13 +24,26 @@ class TeamController < ApplicationController
         else 
             render 'new'
         end
-    end    
-    
-    def show
-        @team = Team.find(params[:id])
     end
 
-private
-    def team_params
-        params.require(:team).permit(:name)
+    def update
+        @team = Team.find(params[:id])
+    
+        if team.update(team_params)
+            redirect_to @team
+        else
+            render 'edit'
+        end
     end
+
+    def destroy
+        
+        @object = Object.find(params[:id])
+        @object.destroy
+    
+        redirect_to objects_path
+
+    private
+        def team_params
+            params.require(:team).permit(:name)
+        end
