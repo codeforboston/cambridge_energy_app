f3b4884e5da8be2f515569bb6d6df28693612bbb
mahtai <matthewahtai@gmail.com>
2015-12-05 21:58:06 -0500

    Update user_building_controller.rb

--- app/controllers/user_building_controller.rb
+++ app/controllers/user_building_controller.rb
@@ -1,7 +1,19 @@
 class UserBuildingController < ApplicationController
     
+    def index
+        @user_building = UserBuilding.all
+    end
+    
+    def show
+        @user_building = UserBuilding.find(params[:id])
+    end
+    
     def new
         @user_building = UserBuilding.new
+    end
+    
+    def edit
+        @user_building = UserBuilding.find(params[:id])
     end
     
     def create
@@ -14,9 +26,20 @@ class UserBuildingController < ApplicationController
         end
     end    
     
-    def show
+    def update
         @user_building = UserBuilding.find(params[:id])
+        
+        if user_building.update(user_building_params)
+            redirect_to @user_buidling
+        else
+            render 'edit'
+        end
     end
+    
+    def destroy
+        @user_building = UserBuidling.find(params[:id])
+        @user_buidling.destroy
+        redirect_to user_building_path(@user_building)
 
 private
     def user_building_params
