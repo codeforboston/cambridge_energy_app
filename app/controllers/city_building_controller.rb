82d408e85db199b1fe8e85e66ee973c5d2a9b802
mahtai <matthewahtai@gmail.com>
2015-12-05 22:08:45 -0500

    Update city_building_controller.rb

--- app/controllers/city_building_controller.rb
+++ app/controllers/city_building_controller.rb
@@ -1,23 +1,8 @@
 class CityBuildingController < ApplicationController
     
-    def new
-        @city_building = CityBuilding.new
-    end
+    @landlord = Landlord.find(params[:landlord_id])
     
-    def create
-        @city_building = CityBuilding.new(city_building_params)
     
-        if @city_building.save
-            redirect_to @city_building
-        else 
-            render 'new'
-        end
-    end    
-    
-    def show
-        @city_building = CityBuilding.find(params[:id])
-    end
-
 private
     def city_building_params
         params.require(:city_building).permit(:landlord_id) #NEEDS ASSESSORS DATA, ENERGY REPORTING INFO
