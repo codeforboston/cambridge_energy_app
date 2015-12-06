2d2129275ccc54d28d476a086fc96c41783898f9
mahtai <matthewahtai@gmail.com>
2015-12-05 22:16:00 -0500

    Update team.rb

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,7 @@
 class Team < ActiveRecord::Base
   validates :name, uniqueness: true
 end
+
+#connection to user not via Rails associated has/belongs_to system, as that seems to require that the teams exist before the users do. 
+#also makes resource routing a mess.
+#maybe fix later.
