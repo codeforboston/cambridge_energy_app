728beff9349662fbe29f7ed2b2aafc54eb78f07b
mahtai <matthewahtai@gmail.com>
2015-12-05 21:54:34 -0500

    Update user_building.rb

--- app/models/user_building.rb
+++ app/models/user_building.rb
@@ -1,5 +1,6 @@
 class UserBuilding < ActiveRecord::Base
   has_many :units
+  has_many :users, through: :units
   validates :address, presence: true
   validates :lat, numericality: true
   validates :lon, numericality: true
