727153d30cec03261f0eb025f4c2bd3b766f5f63
mahtai <matthewahtai@gmail.com>
2015-12-05 22:07:27 -0500

    Update landlord_controller.rb

--- app/controllers/landlord_controller.rb
+++ app/controllers/landlord_controller.rb
@@ -1,9 +1,21 @@
 class LandlordController < ApplicationController
     
+    def index
+        @landlord = Landlord.all
+    end
+
+    def show
+        @landlord = Landlord.find(params[:id])
+    end
+
     def new
         @landlord = Landlord.new
     end
-    
+
+    def edit
+        @landlord = Landlord.find(params[:id])
+    end
+
     def create
         @landlord = Landlord.new(landlord_params)
     
@@ -12,10 +24,24 @@ class LandlordController < ApplicationController
         else 
             render 'new'
         end
-    end    
-    
-    def show
+    end
+
+    def update
         @landlord = Landlord.find(params[:id])
+    
+        if landlord.update(landlord_params)
+            redirect_to @landlord
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        
+        @landlord = Landlord.find(params[:id])
+        @landlord.destroy
+    
+        redirect_to landlords_path
     end
 
 private
