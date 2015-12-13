05bcf8b6a21b8aab8759720dacb766187c07a9f8
Matthew Zagaja <mzagaja@alum.wpi.edu>
2015-12-13 17:01:38 -0500

    Added scaffolding for holding bill data.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :bills
   resources :users
   resources :teams
   resources :units
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'bills#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
