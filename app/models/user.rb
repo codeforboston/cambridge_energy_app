495b7d8739218b950fa81a340af3547779cbf8bc
sonya <sonya.huang@gmail.com>
2015-12-13 22:50:07 -0500

    Add devise to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
   belongs_to :unit
   belongs_to :team
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :teams
   resources :units
   resources :user_buildings
+
+  # devise requires a root
+  root to: "teams#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
