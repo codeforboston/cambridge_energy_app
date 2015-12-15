6c95debbeaf44ec7f484688fb505995d123e683d
mahtai <matthewahtai@gmail.com>
2015-12-15 01:23:04 -0500

    Update users_helper.rb

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
     def profile_complete_meter(user)
       tot = 0
       completeness = ""
-      if user.first_name do tot += 25 end end
+      if user.first_name do tot += 25 end end #add verification for formatting?
       if user.last_name do tot += 25 end end
       if user.street_address do tot += 35 end end
       if user.phone do tot += 15 end end
