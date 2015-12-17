3341c27fa183c51ed137fa74d75c4ab627b6fd20
brisandbeans <bdougsand+cfb@gmail.com>
2015-12-17 17:04:56 -0500

    Fixed syntax error in landing page helper function

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,17 @@
 module UsersHelper
-  
+
     def profile_complete_meter(user)
       tot = 0
       completeness = ""
-      if user.first_name do tot += 25 end end
-      if user.last_name do tot += 25 end end
-      if user.street_address do tot += 35 end end
-      if user.phone do tot += 15 end end
-      if tot < 100 do completeness = "only " + tot + " percent " end end
+      tot += 25 if user.first_name
+      tot += 25 if user.last_name
+      tot += 35 if user.street_address
+      tot += 15 if user.phone
+
+      if tot < 100
+        completeness = "only " + tot + " percent "
+      end
       profile_complete_message = "Your profile is " + completeness + "complete!"
     end
-    
+
 end
