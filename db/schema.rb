ee57ea6d5080e6bba8930317927acf0f1914ad07
Matthew Zagaja <mzagaja@alum.wpi.edu>
2015-12-18 20:00:03 -0500

    Forgot to update schema.rb with my last pull request. Shame on me.

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,16 @@ ActiveRecord::Schema.define(version: 20151214034305) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bills", force: :cascade do |t|
+    t.date     "bill_received"
+    t.decimal  "amount"
+    t.integer  "user_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "bills", ["user_id"], name: "index_bills_on_user_id", using: :btree
+
   create_table "teams", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -74,6 +84,7 @@ ActiveRecord::Schema.define(version: 20151214034305) do
   add_index "users", ["uid"], name: "index_users_on_uid", using: :btree
   add_index "users", ["unit_id"], name: "index_users_on_unit_id", using: :btree
 
+  add_foreign_key "bills", "users"
   add_foreign_key "units", "user_buildings"
   add_foreign_key "users", "teams"
   add_foreign_key "users", "units"
