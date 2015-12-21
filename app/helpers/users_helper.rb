bd1b4102d8d851d053eee9d8eeebcea41e96ad32
mahtai <matthewahtai@gmail.com>
2015-12-21 16:12:04 -0500

    Update users_helper.rb

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,13 +2,18 @@ module UsersHelper
   
     def profile_complete_meter(user)
       tot = 0
-      completeness = ""
-      if user.first_name do tot += 25 end end #add verification for formatting?
-      if user.last_name do tot += 25 end end
-      if user.street_address do tot += 35 end end
-      if user.phone do tot += 15 end end
-      if tot < 100 do completeness = "only " + tot + " percent " end end
-      profile_complete_message = "Your profile is " + completeness + "complete!"
+      if user.first_name 
+        tot += 25 
+      end #add verification for formatting?
+      if user.last_name 
+        tot += 25 
+      end
+      if user.street_address 
+        tot += 35 
+      end
+      if user.phone 
+        tot += 15
+      end
     end
     
 end
