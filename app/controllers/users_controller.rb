90fd42b82698bdeac316686004d2f76cc3e85a05
Matthew Zagaja <mzagaja@alum.wpi.edu>
2016-01-10 18:07:46 -0500

    Syntax change to hopefully fix Heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :street_address, :phone, unit_ids: [], :team_id )
+      params.require(:user).permit(:first_name, :last_name, :street_address, :phone, {unit_ids: []}, :team_id )
     end
 end
