8c56246cf0c32de5738b75c46e54aa11cd595157
Matthew Zagaja <mzagaja@alum.wpi.edu>
2016-02-15 20:18:59 -0500

    Graph now works correctly

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -22,43 +22,39 @@ $.ajax({
  
 function draw(data) {
     var color = d3.scale.category20b();
-    var width = 420,
-        barHeight = 20;
+    var width = 960,
+        height = 500;
+
+    var barWidth = width / data.length;
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
  
-    var x = d3.scale.linear()
-        .range([0, width])
+    var y = d3.scale.linear()
+        .range([height, 0])
         .domain([0, d3.max(data)]);
  
     var chart = d3.select("#graph")
         .attr("width", width)
-        .attr("height", barHeight * data.length);
+        .attr("height", height);
  
     var bar = chart.selectAll("g")
         .data(data)
         .enter().append("g")
-        .attr("transform", function (d, i) {
-                  return "translate(0," + i * barHeight + ")";
-              });
+        .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; })
+        .style("fill", function (d) { return color(d) });
  
     bar.append("rect")
-        .attr("width", x)
-        .attr("height", barHeight - 1)
-        .style("fill", function (d) {
-                   return color(d)
-               })
- 
-    bar.append("text")
-        .attr("x", function (d) {
-                  return x(d) - 10;
-              })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .style("fill", "white")
-        .text(function (d) {
-                  return d;
-              });
+        .attr("y", function (d) { return height - y(d); })
+        .attr("width", barWidth - 1)
+        .attr("height", y);
 }
  
 function error() {
     console.log("error")
+}
+
+function type(d) {
+  d.value = +d.value; // coerce to number
+  return d;
 }
