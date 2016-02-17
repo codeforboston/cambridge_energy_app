58bf8263583adf205124cead25ae84ef2c3d57ee
Matthew Zagaja <mzagaja@alum.wpi.edu>
2016-02-16 21:16:48 -0500

    Added jQuery datepicker to bill comparison front end

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui/datepicker
 //= require foundation
 //= require turbolinks
 //= require_tree .
-
-$(function(){ $(document).foundation(); });
