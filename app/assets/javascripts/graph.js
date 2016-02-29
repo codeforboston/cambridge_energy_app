daf4c5ada188f7f5b82dc9abe6f7d252fd786049
Matthew Zagaja <mzagaja@alum.wpi.edu>
2016-02-28 22:45:22 -0500

    WIP Add Graph Axes
    * Added X and Y axis to the graph
    * X axis currently does not work for unknown reasons

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -31,8 +31,9 @@ $.ajax({
 
 function draw(data, current_user_amount) {
     var color = d3.scale.category20b();
-    var width = parseInt(d3.select(".small-centered").style("width")),
-        height = parseInt(d3.select(".small-centered").style("height"));
+    var margin = {top: 20, right: 30, bottom: 30, left: 40},
+        width = parseInt(d3.select(".small-centered").style("width")) - margin.left - margin.right,
+        height = parseInt(d3.select(".small-centered").style("height")) - margin.top - margin.bottom;
 
     var barWidth = width / data.length;
 
@@ -43,9 +44,19 @@ function draw(data, current_user_amount) {
         .range([height, 0])
         .domain([0, d3.max(data)]);
 
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
     var chart = d3.select("#graph")
-        .attr("width", width)
-        .attr("height", height);
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     var bar = chart.selectAll("g")
         .data(data)
@@ -57,6 +68,15 @@ function draw(data, current_user_amount) {
         .attr("y", y)
         .attr("width", barWidth - 1)
         .attr("height", function(d) { return height - y(d); });
+
+    chart.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    chart.append("g")
+          .attr("class", "y axis")
+          .call(yAxis);
 }
 
 function error() {
