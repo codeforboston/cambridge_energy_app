81924091f161462eb62e3db96d10e7e6c0729dc7
jrpespisa <jrpespisa@verizon.net>
2016-03-08 20:59:10 -0500

    added 3 scenarios

--- spec/features/user_can_create_new_bill_spec.rb
+++ spec/features/user_can_create_new_bill_spec.rb
@@ -1,1 +1,12 @@
 require "rails_helper"
+
+feature "user can add a new bill" do
+  scenario "user successfully adds new bill" do
+  end
+
+  scenario "user does not enter date" do
+  end
+
+  scenario "user does not enter " do
+  end
+end
