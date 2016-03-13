0e193c83062cb273b0e2b178fe92050fd363854e
Matthew Zagaja <mzagaja@alum.wpi.edu>
2016-03-13 16:09:41 -0400

    Revert "add validations and associated model tests for bills"

--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -5,10 +5,4 @@ describe Bill do
   it { should belong_to :user }
   it { should belong_to :unit }
 
-  it { should have_valid(:bill_received).when("2016-02-07") }
-  it { should_not have_valid(:bill_received).when(nil, "", "String") }
-
-  it { should have_valid(:amount).when(0, 9999) }
-  it { should_not have_valid(:amount).when(-10, nil, "", "String", 10000) }
-
 end
