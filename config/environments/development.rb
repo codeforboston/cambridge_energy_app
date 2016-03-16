b837e8b5a1992fc922d8e29a6f884e03ba54e635
mahtai <matthewahtai@gmail.com>
2016-03-15 20:37:51 -0400

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,17 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Care if mailer can't send.
+  # Care if mailer can't send   
   config.action_mailer.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['sendgrid_username'],
+    :password => ENV['sendgrid_password'],
+    :address => "smtp.sendgrid.net",
+    :port => 2525,
+    :domain => "enersave.com",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
