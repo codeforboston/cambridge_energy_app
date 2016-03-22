e0408856c7d70cd0cb97f155c83dd233dcab2cac
Matthew Zagaja <mzagaja@alum.wpi.edu>
2016-03-22 19:53:39 -0400

    Revert "Revert "add validations and associated model tests for bills""

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,4 +2,15 @@ class Bill < ActiveRecord::Base
   belongs_to :user
   belongs_to :unit
   accepts_nested_attributes_for :unit
+
+  validate :bill_received_is_date?
+  validates :bill_received, presence: true
+  validates :amount, presence: true, numericality: true, inclusion: {in: [0, 9999]}
+
+  def bill_received_is_date?
+    unless bill_received.is_a?(Date)
+      errors.add(:bill_received, 'must be a valid date')
+    end
+  end
+
 end
