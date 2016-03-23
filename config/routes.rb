b1b354c1658efd8bf7912ecb4079ffb6f063677d
mahtai <matthewahtai@gmail.com>
2016-03-22 20:43:24 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'bills#new'
 
+  get 'graph/index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
