90b87c4ca06fe328d4378907f031f56836222bb1
Kathleen Votolato <kvotolato3@gmail.com>
2016-04-19 19:58:13 -0400

    Ran migrations

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -101,7 +101,7 @@ class InvitationsController < ApplicationController
       end
     end
   end
-  
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_invitation
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309013348) do
+ActiveRecord::Schema.define(version: 20160411184700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160309013348) do
     t.string   "token"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "mssg"
   end
 
   create_table "teams", force: :cascade do |t|
