e448d64c13c4a44e84fedc6f7dbcc27b0076bb9c
Nathan <nathanrbourke@gmail.com>
2016-04-19 20:42:57 -0400

    complete writing of test

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -1,28 +1,116 @@
 require 'rails_helper'
 
 describe TeamsController do
+  before(:each) { @team = create(:team) }
 
-  before(:each) {
-    @team = create(:team)
-  }
-
-  describe 'GET #index ' do
-
+  describe 'GET #index' do
     context 'when user is signed in' do
-      it do
+      it 'is successful' do
         user = create(:user)
         sign_in user
         get :index
-        is_expected.to respond_with :ok
-        is_expected.to render_template :index
+
+        expect(response).to be_success
+        expect(response).to render_template :index
       end
     end
 
     context 'when user is not signed in' do
-      before { get :index }
-      it { is_expected.to redirect_to(controller: 'devise/sessions', action: :new) }
+      it 'is redirects user to login page' do
+        get :index
+
+        expect(response).to redirect_to(
+          controller: 'devise/sessions',
+          action: :new
+        )
+      end
+    end
+  end
+
+  describe 'GET #new' do
+    context 'when user is signed in' do
+      it 'is successful' do
+        user = create(:user, team: @team)
+        sign_in user
+        get :new
+
+        expect(response).to be_success
+        expect(response).to render_template :new
+      end
     end
 
-    it { expect(@team).to_not be nil }
+    context 'when user is not signed in' do
+      it 'it redirects to user login page' do
+        get :index
+
+        expect(response).to redirect_to(
+          controller: 'devise/sessions',
+          action: :new
+        )
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    context 'when user is signed in' do
+      it 'is successful' do
+        user = create(:user)
+        sign_in user
+        team = attributes_for(:team).merge(user_id: user.id)
+
+        expect{ post(:create, team: team) }.to change{ Team.count }.by(1)
+      end
+    end
+
+    context 'when user is not signed in' do
+      it 'is not successful' do
+        team = attributes_for(:team)
+
+        expect{ post(:create, team: team) }.to change{ Team.count }.by(0)
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'when user is signed in' do
+      it 'is successful' do
+        user = create(:user, team: @team)
+        sign_in user
+        updated_team = { name: 'Team NotSoAwesome' }
+
+        expect{ patch(:update, id: @team.id, team: updated_team) }
+          .to change{ Team.count }.by(0)
+        expect(response).to redirect_to team_path(@team)
+      end
+    end
+
+    context 'when user is not signed in' do
+      it 'is not successful' do
+        updated_team = { name: 'Team NotSoAwesome' }
+
+        expect{ patch(:update, id: @team.id, team: updated_team) }
+          .to change{ Team.count }.by(0)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    context 'user is signed in' do
+      it 'is successful' do
+        user = create(:user)
+        sign_in user
+
+        expect{ delete(:destroy, id: @team.id) }.to change{ Team.count }.by(-1)
+        expect(response).to redirect_to teams_url
+      end
+    end
+
+    context 'user is not signed in' do
+      it 'is not successful' do
+        expect{ delete(:destroy, id: @team.id) }.to change{ Team.count }.by(0)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
   end
 end
