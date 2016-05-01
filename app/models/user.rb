cc52f4eed1d5293e9ae4b6a925a81254822d53e5
Nathan <nathanrbourke@gmail.com>
2016-05-01 16:14:10 -0400

    pick stylesheets

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,15 @@ class User < ActiveRecord::Base
   has_many :bills, dependent: :destroy
   has_many :invitations, foreign_key: "receiver_id", dependent: :destroy
   has_many :senders, through: :invitations
-  
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
+  end
+
+  def first_name_or_email
+    first_name || email
   end
 end
