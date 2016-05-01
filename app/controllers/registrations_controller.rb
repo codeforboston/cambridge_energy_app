a329c8a67ced2707c4af5e1142430c0cae4d2a11
Matthew Zagaja <mzagaja@alum.wpi.edu>
2016-05-01 19:21:58 -0400

    Go to team leaderboard after sign-in
    * Removed unncessary method override
    * Added method to send user to the team dashboard after a sign-in

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,9 +3,4 @@ class RegistrationsController < Devise::RegistrationsController
     super
     current_or_guest_user
   end
-
-  def after_sign_up_path_for(user)
-    'teams/leaderboard'
-  end
 end
-
