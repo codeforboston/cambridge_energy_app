b57fd61af43c441db489ffd50d7d5ecb9a6541ea
Kathleen Votolato <kvotolato3@gmail.com>
2016-05-02 15:40:23 -0400

    Fixed phone number on the user so there is no error when empty or nil

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,19 +11,19 @@ class User < ActiveRecord::Base
   has_many :invitations, foreign_key: "receiver_id", dependent: :destroy
   has_many :senders, through: :invitations
 
-  validates :phone, length: { is: 10 }, if: "!phone.nil?"
-  validates :phone, numericality: { only_integer: true }
+  validates :phone, length: { is: 10 }, if: "phone?"
+  validates :phone, numericality: { only_integer: true }, if: "phone?"
 
   def area_code
-    self.phone.slice(0,3)
+    self.phone == nil ?  (nil) : (self.phone.slice(0,3))
   end
 
   def exchange
-    self.phone.slice(3,3)
+    self.phone == nil ? (nil) : (self.phone.slice(3,3))
   end
 
   def line
-    self.phone.slice(6,4)
+    self.phone == nil ? (nil) : (self.phone.slice(6,4))
   end
 
   def phone_string
