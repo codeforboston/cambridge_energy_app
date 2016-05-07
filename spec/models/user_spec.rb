e40ca84c348a6c5a52072eed3c3e1eb57baab3ee
veddster <matthewvedder@gmail.com>
2016-05-07 17:43:58 -0400

    Include specs for all validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,23 @@ describe User do
   it { should belong_to :team }
   it { should belong_to :unit }
   it { should have_many :bills }
+  it { should have_many :invitations }
+  it { should have_many :senders }
 
+  it 'has a valid factory' do
+  	expect(build(:user)).to be_valid
+  end
+  
+  context 'phone' do
+  	it 'must have 10 characters' do
+  		user = build(:user, phone: '617')
+  		user.valid?
+  		expect(user.errors[:phone]).to include("is the wrong length (should be 10 characters)")
+  	end
+  	it 'should only have numeric characters' do
+  		user = build(:user, phone: '617-555-5555')
+  		user.valid?
+  		expect(user.errors[:phone]).to include("is not a number")
+  	end
+  end
 end
