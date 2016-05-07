829ce47b5b0d1af66830a5c762661456c6006c41
veddster <matthewvedder@gmail.com>
2016-05-07 16:20:06 -0400

    Implement user_building spec

--- spec/models/user_building_spec.rb
+++ spec/models/user_building_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 describe UserBuilding do
+	it { should have_many :units }
+	it 'is invalid without an address' do
+		user_building = build(:user_building, address: nil)
+		user_building.valid?
+		expect(user_building.errors[:address]).to include("can't be blank")
+	end
+	it 'has as many addresses as user_buildings' do
+		expect (UserBuilding.all.count == UserBuilding.all_addresses.count)
+	end
 end
