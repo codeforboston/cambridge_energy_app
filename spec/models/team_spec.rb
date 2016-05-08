2d97fa9ed01b771347ede4c3e0bf2c6c1f08c9c0
veddster <matthewvedder@gmail.com>
2016-05-08 00:20:00 -0400

    Fix invitation dependent destroy bug. Add validation tests

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -3,5 +3,13 @@ require 'rails_helper'
 describe Team do
 
   it { should have_many :users }
+  it { should have_many :invitations }
+  it { should have_many :receivers }
 
+	before(:each) { @team = create(:team) }
+
+  it 'should destroy associated invitations when destroyed' do
+  	@invitation = create(:invitation, sender_id: @team.id)
+  	expect { @team.destroy }.to change { Invitation.count }.by(-1)
+  end
 end
