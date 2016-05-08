f5e0ea8292f2adcd71303bf2fa6f8c4eb07e96ab
veddster <matthewvedder@gmail.com>
2016-05-07 21:45:23 -0400

    Implement tests for User class methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe User do
+	
+	# does this slow performance?
+	before(:each) do
+    @user = create(:user)
+  end
 
   it { should belong_to :team }
   it { should belong_to :unit }
@@ -9,10 +14,10 @@ describe User do
   it { should have_many :senders }
 
   it 'has a valid factory' do
-  	expect(build(:user)).to be_valid
+  	expect(@user).to be_valid
   end
   
-  context 'phone' do
+  describe '.phone' do
   	it 'must have 10 characters' do
   		user = build(:user, phone: '617')
   		user.valid?
@@ -24,4 +29,61 @@ describe User do
   		expect(user.errors[:phone]).to include("is not a number")
   	end
   end
+	
+	describe '.area_code' do
+		it 'should return area code' do
+  		expect(@user.area_code).to eq '123'
+ 		end
+ 	end
+
+ 	describe '.exchange' do
+ 		it 'should return exchange' do
+ 			expect(@user.exchange).to eq '456'
+ 		end
+ 	end
+
+ 	describe '.line' do
+ 		it 'should return line' do
+ 			expect(@user.line).to eq '7890'
+ 		end
+ 	end
+
+ 	describe '.phone_string' do
+ 		it 'should return phone number with dashes inserted' do
+ 			expect(@user.phone_string).to eq '123-456-7890'
+ 		end
+ 	end
+
+ 	describe '.first_name_or_email' do
+ 		context 'first name is nil' do
+ 			it 'should return .email_without_domain' do
+ 				@user.update(first_name: nil)
+ 				# .send okay to do?
+ 				expect(@user.first_name_or_email).to eq @user.send(:email_without_domain)
+ 			end
+ 		end
+ 		context 'first name is empty' do
+ 			it 'should return .email_without_domain' do
+ 				@user.update(first_name: '')
+ 				# .send okay to do?
+ 				expect(@user.first_name_or_email).to eq @user.send(:email_without_domain)
+ 			end
+ 		end
+ 		context 'first name is neither nil or empty' do
+ 			it 'should return first_name' do
+ 				expect(@user.first_name).to eq 'Bob'
+ 			end
+ 		end
+ 	end
+
+ 	describe '.email_without_domain' do
+ 		it 'should return characters before @ sign in email' do
+ 			@user.update(email: 'bruce@wayne.com')
+ 			# .send okay to do?
+ 			expect(@user.send(:email_without_domain)).to eq 'bruce'
+ 		end
+ 	end
+
+ 	# write test for actual score computation
+ 	# Write test for OmniAuth
 end
