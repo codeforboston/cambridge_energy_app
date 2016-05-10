8fedac11928db4c8fc04393a76cc155154292c1c
veddster <matthewvedder@gmail.com>
2016-05-09 21:14:19 -0400

    refactor user_building spec

--- spec/models/user_building_spec.rb
+++ spec/models/user_building_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 describe UserBuilding do
-	it { should have_many :units }
-	it 'is invalid without an address' do
-		user_building = build(:user_building, address: nil)
-		user_building.valid?
-		expect(user_building.errors[:address]).to include("can't be blank")
-	end
-	it 'has as many addresses as user_buildings' do
-		expect (UserBuilding.all.count == UserBuilding.all_addresses.count)
-	end
+  it { should have_many :units }
+  it { should validate_presence_of(:address) }
+
+  describe '.all_addresses' do
+    it 'returns as many addresses as there are user_buildings' do
+      expect(UserBuilding.all.count).to eq UserBuilding.all_addresses.count
+    end
+  end
 end
