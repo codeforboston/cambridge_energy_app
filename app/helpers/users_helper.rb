978ed198bb49f3b31712d6dfbe1b4b607a6d9902
Matthew Zagaja <mzagaja@alum.wpi.edu>
2016-05-16 22:39:25 -0400

    Change profile complete text to HTML progress bar
    * Adds progress bar to user profile view along with percentage text

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,19 +1,13 @@
 module UsersHelper
 
     def profile_complete_meter(user)
-      tot = 0
-      completeness = ""
-      tot += 25 unless user.first_name.nil? || user.first_name.empty?
-      tot += 25 unless  user.last_name.nil? || user.last_name.empty?
-      tot += 35 if user.unit.present?
-      tot += 15 unless user.phone.nil? || user.phone.empty?
+      complete_percentage = 0
+      complete_percentage += 25 unless user.first_name.nil? || user.first_name.empty?
+      complete_percentage += 25 unless  user.last_name.nil? || user.last_name.empty?
+      complete_percentage += 35 if user.unit.present?
+      complete_percentage += 15 unless user.phone.nil? || user.phone.empty?
 
-      if tot == 0
-        completeness = "0 percent "
-      elsif tot < 100
-        completeness = "only " + tot.to_s + " percent "
-      end
-      profile_complete_message = "Your profile is " + completeness + "complete!"
+      complete_percentage
     end
 
     def devise_edit_user_path
