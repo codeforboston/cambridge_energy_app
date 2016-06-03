761d4db9620ba741ac593bbfbcd92b7cc47b57d2
veddster <matthewvedder@gmail.com>
2016-06-03 19:22:10 -0400

    Implement integration test for inviting existing user

---
+++ spec/features/invite_existing_user.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+require "spec_helper"
+include Warden::Test::Helpers
+
+feature "user can invite existing user to join team" do
+  scenario "user successfully invites existing user" do
+    user = create(:user)
+    # create another user to send invitation to
+    create(:user, email: "existing@example.com")
+    puts user.email
+    login_as user, :scope => :user
+    visit "/users/invitation/new"
+    fill_in "Email", with: "existing@example.com"
+    #click button: send an invitation
+    find('input[name="commit"]').click
+    expect(page).to have_content "existing@example.com has been invited to join your team"
+  end
+end
