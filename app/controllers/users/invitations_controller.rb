c41db2dfdc93e12228770c86425b9ee05b7464f7
veddster <matthewvedder@gmail.com>
2016-06-11 21:20:54 -0400

    Modify invitations#create to not send email if user exists

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,26 +1,25 @@
 class Users::InvitationsController < Devise::InvitationsController
 
+  # POST /resource/invitation
+  def create
+    @user = User.find_by(email: invite_params[:email])
+
+    if @user && @user.email != current_user.email
+      self.resource = invite_resource_and_skip
+    else
+      self.resource = invite_resource
+    end
+
+    super
+  end
+
   protected
 
-  # invite_resource is called when creating invitation
-  # should return an instance of resource class
-
-  # this is devise_invitable's implementation
-  # def invite_resource(&block)
-  #   resource_class.invite!(invite_params, current_inviter, &block)
-  # end
-
-  def invite_resource(&block)
-    @user = User.find_by(email: invite_params[:email])
-    # @user is an instance or nil
-    if @user && @user.email != current_user.email
-      # invite! instance method returns a Mail::Message instance
-      @user.invite!(current_user)
-      # return the user instance to match expected return type
-      @user
-    else
-      # invite! class method returns invitable var, which is a User instance
-      resource_class.invite!(invite_params, current_inviter, &block)
+  def invite_resource_and_skip
+    @user.invite!(current_user) do |u|
+      # skip sending emails on invite
+      u.skip_invitation = true
     end
+    @user
   end
 end
