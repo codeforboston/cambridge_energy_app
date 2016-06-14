2c89b403f614bf37c87fe174199850560822184e
veddster <matthewvedder@gmail.com>
2016-06-14 01:13:47 -0400

    Override #create in invitable controller to use our custom methods for existing users

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -2,24 +2,17 @@ class Users::InvitationsController < Devise::InvitationsController
 
   # POST /resource/invitation
   def create
-    @user = User.find_by(email: invite_params[:email])
-
-    if @user && @user.email != current_user.email
-      self.resource = invite_resource_and_skip
+    user = User.find_by(email: invite_params[:email])
+    if user && user.email != current_user.email
+      if user.team_id != current_user.team_id
+        current_user.invite_existing(user)
+        flash[:notice] = "#{user.first_name_or_email} has been invited to join your team!"
+      else
+        flash[:notice] = "#{user.first_name_or_email} is already on your team!"
+      end
+      respond_with user, :location => after_invite_path_for(user)
     else
-      self.resource = invite_resource
+      super
     end
-
-    super
-  end
-
-  protected
-
-  def invite_resource_and_skip
-    @user.invite!(current_user) do |u|
-      # skip sending emails on invite
-      u.skip_invitation = true
-    end
-    @user
   end
 end
