da9d6e039e263172981d5176abc6742d9c2424a9
veddster <matthewvedder@gmail.com>
2016-06-15 20:04:23 -0400

    Alter score spec to account for teams with some members without bills

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 describe Team do
 
-  before(:each) do
-    @team = create(:team)
-  end
+let(:team) { create :team }
 
   context "Validations" do
     it { should validate_presence_of :name }
@@ -15,17 +13,20 @@ describe Team do
   end
 
   it 'has a valid factory' do
-    expect(@team).to be_valid
+    expect(team).to be_valid
   end
 
   describe '.score' do
-    let(:team) { create(:team_with_members) }
-    it 'should return a score for the team if it has sufficient bills' do
-      expect(team.score).not_to eq 0
+    let(:team_with_score) { create :team_with_members }
+    let(:user) { create(:user, team: team_with_members, bills: []) }
+
+    it 'should return a score if at least one team member has sufficient bills' do
+      # RE-RUN: if the factory randomly selects bills with similar values test fails"
+      expect(team_with_score.score).not_to eq 0
     end
 
     it 'should return 0 if the team lacks sufficient bills' do
-      expect(@team.score).to eq 0
+      expect(team.score).to eq 0
     end
   end
 end
