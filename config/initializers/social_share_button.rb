55355340fee6d0abe94cdafa41f8cbf43ee6d556
mahtai <matthewahtai@gmail.com>
2016-06-28 17:45:11 -0400

    Paring down sites

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,3 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook google_plus google_bookmark delicious tumblr plurk pinterest email linkedin)
+  config.allow_sites = %w(twitter facebook pinterest linkedin)
 end
