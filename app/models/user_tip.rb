c704626ef9a490f48d76ab06933ac22bd3b586e0
mahtai <matthewahtai@gmail.com>
2016-06-28 20:43:35 -0400

    Vote uniqueness

--- app/models/user_tip.rb
+++ app/models/user_tip.rb
@@ -4,5 +4,6 @@ class UserTip < ActiveRecord::Base
 
   validates :user, presence: true
   validates :result, presence: true
-  
+  validates :tip, presence: true
+  validates_uniqueness_of :user_id, :scope => [:tip_id]
 end
