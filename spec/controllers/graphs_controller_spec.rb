39235112f3c15934670239f05ef2dff6466a948a
Mr. Is <isaac.ardis@gmail.com>
2016-07-05 10:27:09 -0400

    be more specific

--- spec/controllers/graphs_controller_spec.rb
+++ spec/controllers/graphs_controller_spec.rb
@@ -11,6 +11,9 @@ describe GraphController do
     it 'is successful' do
       get(:index)
       expect(assigns(:user)).not_to be_nil
+
+      # And make sure we're assigning the right person (current_user).
+      expect(assigns(:user).id).to eq(@user.id)
     end
   end
 
