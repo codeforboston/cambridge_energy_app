3545dc711bac8d7b2290a9cd6ce6f745b73064fc
Frank Cardillo <facardillo91@gmail.com>
2016-08-24 15:56:57 -0400

    Fixed dropdown bar not appearing after first link_to action is triggered

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+document.addEventListener("turbolinks:load", function() {
   $("#dropdown-hover").hover(function() {
     $("#name-dropdown-list").toggleClass("display-none");
   });
