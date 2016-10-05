9623291e49decf6fb3538a3b704c72d74e25a270
Shaun Carland <shaun@Shauns-MacBook-Pro.local>
2016-10-05 15:36:33 -0400

    Refactor model specs.

--- spec/models/user_building_spec.rb
+++ spec/models/user_building_spec.rb
@@ -5,7 +5,7 @@ describe UserBuilding do
   it { should validate_presence_of(:address) }
 
   before(:each) do
-    UserBuilding.destroy_all # Clear out any newly-existing find-or-generate buildings. 
+    UserBuilding.destroy_all
   	@user_building = create(:user_building)
   end
 
@@ -17,7 +17,7 @@ describe UserBuilding do
 
   describe '.find_or_generate' do
 
-  	it 'should return a building by matching id' do 
+  	it 'should return a building by matching id' do
   		b = UserBuilding.find_or_generate(@user_building.id, nil)
   		expect(b).to eq(@user_building)
   	end
@@ -27,55 +27,54 @@ describe UserBuilding do
   		expect(b).to eq(@user_building)
   	end
 
-  	[
-	  	'123 main st, cambridge, ma',
-	  	'123 main st cambridge ma', #fail
-	  	'123 Main st., Cambridge MA',
-	  	'123 main st., Cambridge, Massachussets 02138', #fail
-	  	'123 main st., Cambrdige, Masachussetts', #fail
-	  	'123 main st cambridge, massachussetts' #fail
-	  ].each do |address|
+    context 'when the address is in a fuzzy format' do
+      fuzzy_addresses =
+    	[
+  	  	'123 main st, cambridge, ma',
+  	  	'123 main st cambridge ma', #fail
+  	  	'123 Main st., Cambridge MA',
+  	  	'123 main st., Cambridge, Massachussets 02138', #fail
+  	  	'123 main st., Cambrdige, Masachussetts', #fail
+  	  	'123 main st cambridge, massachussetts' #fail
+  	  ]
 
-  		it "should return match or create by fuzzily-matching address '#{address}'" do
-  			match = UserBuilding.find_or_generate(nil, address) 	
-  			
-        # Use this to see which addresses pass/fail.
-        # expect(match).to eq(@user_building) 
-        
-        # Expect match to be included in either the first (and only) building id or 
-        # in the last building id. 
-        expect([@user_building.id, UserBuilding.last.id]).to include(match.id)
-        
-        # And just to be sure we're solid at our expected building count.
-        expect([1,2]).to include(UserBuilding.count)
-  		end
-  		
-  		# => Failed examples:
+      fuzzy_addresses.each do |address|
+    		it "should return match or create by fuzzily-matching address '#{address}'" do
+    			match = UserBuilding.find_or_generate(nil, address)
 
-  		# rspec './spec/models/user_building_spec.rb[1:4:4]' # UserBuilding.find_or_generate should return match by address '123 main st cambridge ma'
-  		# rspec './spec/models/user_building_spec.rb[1:4:6]' # UserBuilding.find_or_generate should return match by address '123 main st., Cambridge, Massachussets 02138'
-  		# rspec './spec/models/user_building_spec.rb[1:4:7]' # UserBuilding.find_or_generate should return match by address '123 main st., Cambrdige, Masachussetts'
-  		# rspec './spec/models/user_building_spec.rb[1:4:8]' # UserBuilding.find_or_generate should return match by address '123 main st cambridge, massachussetts'
+          # Use this to see which addresses pass/fail.
+          # expect(match).to eq(@user_building)
+
+          # Expect match to be included in either the first (and only) building id or
+          # in the last building id.
+          expect([@user_building.id, UserBuilding.last.id]).to include(match.id)
+
+          # And just to be sure we're solid at our expected building count.
+          expect([1,2]).to include(UserBuilding.count)
+    		end
+    		# => Failed examples:
+
+    		# rspec './spec/models/user_building_spec.rb[1:4:4]' # UserBuilding.find_or_generate should return match by address '123 main st cambridge ma'
+    		# rspec './spec/models/user_building_spec.rb[1:4:6]' # UserBuilding.find_or_generate should return match by address '123 main st., Cambridge, Massachussets 02138'
+    		# rspec './spec/models/user_building_spec.rb[1:4:7]' # UserBuilding.find_or_generate should return match by address '123 main st., Cambrdige, Masachussetts'
+    		# rspec './spec/models/user_building_spec.rb[1:4:8]' # UserBuilding.find_or_generate should return match by address '123 main st cambridge, massachussetts'
+      end
   	end
   end
 
-  describe '.parse_and_save_address_granules' do 
-    # Note that this tests only a well-formed address, and thus
-    # signifies only that the components are being parsed and saved, 
-    # not their being parsed well.
-    it 'should save parsed address components via after_save callback' do 
+  describe '.parse_and_save_address_granules' do
+    context 'when the address is in a valid format' do
       new_address = '115 Prospect St, Cambridge, MA'
       new_build_attrs = {address: new_address}
       created = UserBuilding.create(new_build_attrs)
 
-      # Basic address did save.
-      expect(created).to have_attributes(address: new_address)
-
-      # Parsed address components. 
-      expect(created.number).to_not be_nil
-      expect(created.street).to_not be_nil
-      expect(created.city).to_not be_nil
-      expect(created.state).to_not be_nil
+      it 'saves parsed address components via after_save callback' do
+        expect(created).to have_attributes(address: new_address)
+        expect(created.number).to_not be_nil
+        expect(created.street).to_not be_nil
+        expect(created.city).to_not be_nil
+        expect(created.state).to_not be_nil
+      end
     end
   end
 end
